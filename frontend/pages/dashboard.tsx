98cd7783a9224bfef9cf2976f3bd2723fd26a35b
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2023-06-01 12:37:40 +0200

    Update dashboard.tsx

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,9 +1,27 @@
 import { AuthenticationTitle } from '../src/components/AuthenticationTitle/AuthenticationTitle';
-
+import {Container} from '@mantine/core';
 export default function HomePage() {
   return (
     <>
-      Dashboard!
+    <Container style={{maxWidth: '71rem'}}>
+    <iframe style={{width: '100%', height: '90vh', justifyContent: 'center'}} srcDoc="<html>
+  <head>
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='https://cdn.form.io/formiojs/formio.full.min.css'>
+    <script src='https://cdn.form.io/formiojs/formio.full.min.js'></script>
+    <script type='text/javascript'>
+      window.onload = function() {
+        Formio.builder(document.getElementById('builder'), {}, {});
+      };
+    </script>
+  </head>
+  <body>
+    <div id='builder'></div>
+  </body>
+</html>
+"> </iframe>
+
+      </Container>
     </>
   );
 }
