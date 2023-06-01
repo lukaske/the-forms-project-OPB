8192b3a9e0cd80d63465cf3650a6d2e4014ea4b5
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2023-06-01 12:51:58 +0200

    Update settings.py

--- formsProject/settings.py
+++ formsProject/settings.py
@@ -149,8 +149,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'app/static'),
-    '/app/static'
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
